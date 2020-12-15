64513fc9893eca6927d0a58d63ac0be48a650801
gooseisdead <gooseisdead@mac.com>
2020-12-15 11:37:31 -0500

    initial commit

---
+++ src/Components/FreeAgent.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import BidForm from '../Components/BidForm'
+
+class FreeAgent extends React.Component {
+
+
+    render () {
+        const { freeagent } = this.props;
+        return (
+            
+            <div className="freeagent">
+                <div className="facard">{freeagent.position} {freeagent.name}<br></br>
+                    ${freeagent.contract_value} million ({freeagent.contract_length})<br></br>
+                    <BidForm />
+                </div>
+            </div>
+            
+        )
+    }
+}
+    
+export default FreeAgent;
---
+++ src/Containers/FreeAgentContainer.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import FreeAgent from '../Components/FreeAgent'
+import Header from '../Components/Header'
+
+class FreeAgentContainer extends React.Component {
+
+    state = {
+        api: []
+    }
+    
+    componentDidMount() {
+        fetch("http://localhost:8000/players")
+        .then(resp => resp.json())
+        .then(data => this.setState( {api: data} ))
+    }
+
+    // handleNewPlayer = (playerObj) => {
+    //     fetch("http://localhost:8000/players", {
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+            
+    //       },
+    //       body: JSON.stringify(playerObj)
+    //     })
+    //     .then(resp => resp.json())
+    //     .then(newPlayer => this.setState({ api: [...this.state.api, newPlayer]}))
+    //   }
+      
+    renderPlayers = () => {
+        let filteredArray = this.state.api
+        return filteredArray.map(el => <FreeAgent key={el.id} freeagent={el} api={this.state.api} />)
+    }
+
+    
+
+    // totalSalary = () => {
+    //     let salaryArray = this.state.api.filter(function (el) {
+    //         return el.contract_value
+    //     })
+    //     let grandTotal = []
+    //     salaryArray.map(el => grandTotal.push(el.contract_value))
+    //     // grandTotal.reduce((a, b) => a + b, 0)
+    //     grandTotal.reduce((a, b) => a + b, 0)
+    //     console.log(grandTotal.reduce((a, b) => a + b, 0))
+    // };
+        
+    // renderByTeam = () => {
+    //     let teamFilteredArray = this.state.api.filter(function (el) {
+    //         return el.current_team === "Yankees"          
+    //     });
+    //       console.log(teamFilteredArray);
+    // }
+
+    render() {
+        return (
+            <div className="team-container">
+                <Header />
+                {/* <h2 onClick={this.totalSalary}>What is the Salary?</h2> */}
+                {/* <h2 onClick={this.renderByTeam}>Where are Yankees?</h2> */}
+                {this.renderPlayers()}
+                {/* <NewPlayerForm handleNewPlayer={this.handleNewPlayer}/> */}
+            </div>
+        )
+    }
+}
+
+export default FreeAgentContainer
